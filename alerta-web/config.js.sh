b846a9288d76d111623141470133748c5719d4e1
Tim Allen <tiallen@softlayer.com>
2016-06-10 14:13:20 -0500

    Use ash instead of bash for config.js.sh

--- alerta-web/config.js.sh
+++ alerta-web/config.js.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/ash
 
 cat >/app/config.js << EOF
 'use strict';
 
 angular.module('config', [])
   .constant('config', {
-    'endpoint'    : "/api",
+    'endpoint'    : "$ENDPOINT",
     'provider'    : "$PROVIDER",
     'client_id'   : "$CLIENT_ID",
     'gitlab_url'  : "$GITLAB_URL",
-
     'colors'      : {}
   });
 EOF
